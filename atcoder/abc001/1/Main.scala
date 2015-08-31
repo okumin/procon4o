1d8921e8a72f09b9a69fec2c03a81bf16b5abb59
okumin <mail@okumin.com>
2015-08-31 20:19:54 +0900

    Solve abc001_1

---
+++ atcoder/abc001/1/Main.scala
@@ -0,0 +1,10 @@
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val scanner = new java.util.Scanner(System.in)
+    val h1 = scanner.nextInt()
+    val h2 = scanner.nextInt()
+    println(h1 - h2)
+  }
+}
+
