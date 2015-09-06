dff862b9f1c7e03cf1e970f19dc337113bc322c5
okumin <mail@okumin.com>
2015-09-06 18:54:30 +0900

    Refactor abc001_3

--- atcoder/abc001/3/Main.scala
+++ atcoder/abc001/3/Main.scala
@@ -1,25 +1,9 @@
 
 object Main {
-  def hoi(deg: Int): String = deg match {
-    case x if x <= 112 => "N"
-    case x if x > 112 && x <= 337 => "NNE"
-    case x if x <= 562 => "NE"
-    case x if x <= 787 => "ENE"
-    case x if x <= 1012 => "E"
-    case x if x <= 1237 => "ESE"
-    case x if x <= 1462 => "SE"
-    case x if x <= 1687 => "SSE"
-    case x if x <= 1912 => "S"
-    case x if x <= 2137 => "SSW"
-    case x if x <= 2362 => "SW"
-    case x if x <= 2587 => "WSW"
-    case x if x <= 2812 => "W"
-    case x if x <= 3037 => "WNW"
-    case x if x <= 3262 => "NW"
-    case x if x <= 3487 => "NNW"
-    case _ => "N"
-  }
-  def furyoku(w: Int): Int = math.round(w * 10.0 / 60.0) match {
+  val hois: Array[String] = Array("N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW")
+  def hoi(deg: Int): String = hois(((deg * 10 + 1125) / 2250) % 16)
+
+  def furyoku(dis: Int): Int = math.round(dis * 10.0 / 60.0) match {
     case x if x <= 2 => 0
     case x if x <= 15 => 1
     case x if x <= 33 => 2
@@ -38,15 +22,15 @@ object Main {
   def main(args: Array[String]): Unit = {
     val scanner = new java.util.Scanner(System.in)
     val deg = scanner.nextInt()
-    val w = scanner.nextInt()
-    val (h, f) = solve(deg, w)
-    println(h + " " + f)
+    val dis = scanner.nextInt()
+    val (dir, w) = solve(deg, dis)
+    println(dir + " " + w)
   }
 
-  def solve(deg: Int, w: Int): (String, Int) = {
-    val f = furyoku(w)
-    val h = if (f == 0) "C" else hoi(deg)
-    (h, f)
+  def solve(deg: Int, dis: Int): (String, Int) = {
+    val w = furyoku(dis)
+    val dir = if (w == 0) "C" else hoi(deg)
+    (dir, w)
   }
 }
 
