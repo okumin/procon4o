e34fb78e217012851d2f1a5f36ea46d8d8a5491c
okumin <mail@okumin.com>
2015-10-24 22:16:10 +0900

    Solve ITP1_4_B

---
+++ aoj/ITP1/4_B/main.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    double r;
+    cin >> r;
+    double pi = 3.14159265359;
+    printf("%f %f\n", r * r * pi, 2 * r * pi);
+    return 0;
+}
+
